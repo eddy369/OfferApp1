6affa0f605911816d8e22714a904e5c7410fcb8c
andreaPerez19 <andre.g0397@gmail.com>
2021-11-20 13:42:43 -0500

    cambios catalogo

---
+++ OfferApp1/DatosEmpresa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OfferApp1
+{
+    class DatosEmpresa
+    {
+        public int ID_USUARIO { get; set; }
+        public int ID_EMPRESA { get; set; }
+        public string NOMBRE { get; set; }
+        public string TELEFONO { get; set; }
+        public string DIRECCION { get; set; }
+        public string RAZON_SOCIAL { get; set; }
+        public string EMAIL_EMPRESA { get; set; }
+        public string CIUDAD { get; set; }
+        public string LOGO { get; set; }
+
+    }
+}
---
+++ OfferApp1/viewCatalogo.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OfferApp1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class viewCatalogo : ContentPage
+    {
+        private const string Url = "http://192.168.100.245/offerApp/postEmpresa.php";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<OfferApp1.DatosEmpresa> _post;
+        public viewCatalogo()
+        {
+            InitializeComponent();
+            get();
+        }
+
+        private async void get()
+        {
+            var content = await client.GetStringAsync(Url);
+            List<OfferApp1.DatosEmpresa> post = JsonConvert.DeserializeObject<List<OfferApp1.DatosEmpresa>>(content);
+            _post = new ObservableCollection<OfferApp1.DatosEmpresa>(post);
+
+            MyListView.ItemsSource = _post;
+
+        }
+
+    }
+}
