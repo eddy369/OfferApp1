f6c0d422967a6be9fca0af4d407751ba5999e9b7
andreaPerez19 <andre.g0397@gmail.com>
2021-11-26 01:47:52 -0500

    final

--- OfferApp1.Android/Resources/Resource.designer.cs
+++ OfferApp1.Android/Resources/Resource.designer.cs
@@ -17138,25 +17138,28 @@ namespace OfferApp1.Droid
 			public const int prueba = 2131165337;
 			
 			// aapt resource value: 0x7F07009A
-			public const int tenis = 2131165338;
+			public const int prueba2 = 2131165338;
 			
 			// aapt resource value: 0x7F07009B
-			public const int test_custom_background = 2131165339;
+			public const int tenis = 2131165339;
 			
 			// aapt resource value: 0x7F07009C
-			public const int tooltip_frame_dark = 2131165340;
+			public const int test_custom_background = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int tooltip_frame_light = 2131165341;
+			public const int tooltip_frame_dark = 2131165341;
 			
 			// aapt resource value: 0x7F07009E
-			public const int wallpaper = 2131165342;
+			public const int tooltip_frame_light = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int wallpaper2 = 2131165343;
+			public const int wallpaper = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int wallpaperlego = 2131165344;
+			public const int wallpaper2 = 2131165344;
+			
+			// aapt resource value: 0x7F0700A1
+			public const int wallpaperlego = 2131165345;
 			
 			static Drawable()
 			{
